15726220d9999c004e4d15fa67985bdfd3082574
motxx <motohiko.ave@gmail.com>
2024-02-23 17:29:40 +0900

    feat: use masonry

--- src/components/ImageGallary.tsx
+++ src/components/ImageGallary.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Masonry from 'react-masonry-css'
 import ImageCard from './ImageCard';
 import { Note } from '../domain/entities/note';
 import { NoteService } from '../services/note-service';
 import { UserService } from '../services/user-service';
 import { WalletService } from '../services/wallet-service';
+import './masonry.scss';
 
 export interface ImageGalleryProps {
   notes: Note[];
@@ -13,21 +15,34 @@ export interface ImageGalleryProps {
 }
 
 const ImageGallery: React.FC<ImageGalleryProps> = (props) => {
+  const breakpointCols = {
+    default: 4,
+    1350: 3,
+    1048: 2,
+    576: 1,
+  }
+
   return (
     <div className="container mx-auto px-4 py-8">
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+      <Masonry
+        breakpointCols={breakpointCols}
+        className="masonry-grid"
+        columnClassName="masonry-grid-column"
+      >
         {props.notes.map((note) => (
-          <ImageCard
-            key={note.id}
-            noteId={note.id}
-            text={note.text}
-            imageUrl={note.imageUrl}
-            noteService={props.noteService}
-            userService={props.userService}
-            walletService={props.walletService}
-          />
+          <div className="image-card">
+            <ImageCard
+              key={note.id}
+              noteId={note.id}
+              text={note.text}
+              imageUrl={note.imageUrl}
+              noteService={props.noteService}
+              userService={props.userService}
+              walletService={props.walletService}
+            />
+          </div>
         ))}
-      </div>
+      </Masonry>
     </div>
   );
 };
